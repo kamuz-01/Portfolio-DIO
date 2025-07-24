88630071b37f37b9e23ca708724155bc0dfb9d30
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2025-07-24 17:45:18 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-const url = 'https://raw.githubusercontent.com/kamuz-01/Portfolio-DIO/main/Portfolio-DIO/js-developer-portfolio-main/data/profile.json';
+const url = 'https://raw.githubusercontent.com/kamuz-01/Portfolio-DIO/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
